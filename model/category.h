1b02b67daf9c00e93396fbce027149a8ee7721c0
senx <>
2024-05-16 09:48:32 +0800

    fix bug

--- model/category.h
+++ model/category.h
@@ -9,13 +9,13 @@ class Category {
 public:
     Category();
 
-    uint id;
-    QString name;
-    uint total;
-    uint i;
-    uint x;
-    QString m;
-    bool jm;
+    uint id = 0;
+    QString name = "";
+    uint total = 0;
+    uint i = 0;
+    uint x = 0;
+    QString m = "";
+    bool jm = false;
 
     QString toString();
     QVariantMap toVMap();
