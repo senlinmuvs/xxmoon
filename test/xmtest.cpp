4914955fd10cf9e7d5bf19d0f71ce7bc7062bee3
senx <>
2024-06-01 07:23:38 +0800

    删除无用文件

--- test/xmtest.cpp
+++
@@ -1,9 +0,0 @@
-#include "xmtest.h"
-
-void XMTest::test_case1() {
-    QVERIFY(true); // 确保条件为真
-}
-
-void XMTest::test_case2() {
-    QCOMPARE(1 + 1, 2); // 比较两个值是否相等
-}
--- test/xmtest.h
+++
@@ -1,8 +0,0 @@
-#include <QtTest/QtTest>
-
-class XMTest : public QObject {
-    Q_OBJECT
-private slots:
-    void test_case1();
-    void test_case2();
-};
